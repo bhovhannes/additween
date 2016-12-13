c199fcbbe5fae4713ad0a5dca122698ee11d8b39
Hovhannes Babayan <bhovhannes@gmail.com>
2016-12-13 12:28:11 +0400

    chore: adding dummy test

---
+++ test/typings.spec.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+'use strict'
+
+var expect = require('expect.js')
+
+describe('additween typings', function() {
+   it('should compile without error', function() {
+       expect(true).to.be(true)
+   })
+})
